d184ed1705f2325d781dd9e81dda6bac741a32bb
alexander-morris <alex@theblockchaininstitute.org>
2019-09-25 14:37:45 -0500

    adding general functionality framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor ( props ) {
+      super(props)
+      this.state = {
+        'img' : props.img,
+        'tips' : props.tips
+
+      }
+      console.log('state is', this.state)
+     
+  }
+
+  render () {
+    var tips = this.state.tips.map((tip) =>
+      <div class="tooltip" style={{ left: tip.x + "%", top: tip.y + "%" }}> 
+          <span class="tooltiptext">{ tip.text }</span>
+      </div>
+
+    );
+    return (
+      <div className="App">
+          <img src={this.state.img} className="background" alt="logo" />
+          <div class="overlay">
+            {tips}
+          </div>
+      </div>
+    );
+  }
+
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from './logo.svg';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const tips = [{
+	text 	: "Tip 1",
+	x		: 22.34,
+	y 		: 20.12
+},{
+	text 	: "Tip 2",
+	x		: 12.34,
+	y 		: 38.12
+},{
+	text 	: "Tip 3",
+	x		: 75.34,
+	y 		: 80.12
+}];
+
+ReactDOM.render(<App img={logo} tips={tips}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
