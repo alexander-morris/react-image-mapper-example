88859b710c1b01bdd657a22b2b4c3eaa041e7d94
alexander-morris <alex@theblockchaininstitute.org>
2019-09-26 11:17:53 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,108 @@
 import React from 'react';
-import './App.css';
+import ImageMapper from 'react-image-mapper';
+// import './App.css';
+
 
 class App extends React.Component {
-
-  constructor ( props ) {
-      super(props)
-      this.state = {
-        'img' : props.img,
-        'tips' : props.tips
-
-      }
-      console.log('state is', this.state)
-     
+  constructor (props) {
+    super(props);
+    this.state = {
+      img : props.img,
+      map : props.map
+    }
   }
 
   render () {
-    var tips = this.state.tips.map((tip) =>
-      <div class="tooltip" style={{ left: tip.x + "%", top: tip.y + "%" }}> 
-          <span class="tooltiptext">{ tip.text }</span>
-      </div>
-
-    );
-    return (
-      <div className="App">
-          <img src={this.state.img} className="background" alt="logo" />
-          <div class="overlay">
-            {tips}
+    return(
+          <div style={{ position: "relative" }}>
+            <ImageMapper
+              src={this.state.img}
+              map={this.state.map}
+              width={500}
+              onLoad={() => this.load()}
+              onClick={area => this.clicked(area)}
+              onMouseEnter={area => this.enterArea(area)}
+              onMouseLeave={area => this.leaveArea(area)}
+              onMouseMove={(area, _, evt) => this.moveOnArea(area, evt)}
+              onImageClick={evt => this.clickedOutside(evt)}
+              onImageMouseMove={evt => this.moveOnImage(evt)}
+              lineWidth={4}
+              strokeColor={"white"}
+            />
+            {this.state.hoveredArea && (
+              <span
+                className="tooltip"
+                style={{ ...this.getTipPosition(this.state.hoveredArea) }}
+              >
+                {this.state.hoveredArea && this.state.hoveredArea.name}
+              </span>
+            )}
           </div>
-      </div>
     );
+
   }
 
+  getTipPosition(area) {
+    return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
+  }
+
+  getInitialState() {
+    return { hoveredArea: null, msg: null, moveMsg: null };
+  }
+
+  load() {
+    this.setState({ msg: "Interact with image !" });
+  }
+
+  clicked(area) {
+    this.setState({
+      msg: `You clicked on ${area.shape} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  }
+
+  clickedOutside(evt) {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    this.setState({
+      msg: `You clicked on the image at coords ${JSON.stringify(coords)} !`
+    });
+  }
+
+  moveOnImage(evt) {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    this.setState({
+      moveMsg: `You moved on the image at coords ${JSON.stringify(coords)} !`
+    });
+  }
+
+  enterArea(area) {
+    this.setState({
+      hoveredArea: area,
+      msg: `You entered ${area.shape} ${area.name} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  }
+
+  leaveArea(area) {
+    this.setState({
+      hoveredArea: null,
+      msg: `You leaved ${area.shape} ${area.name} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  }
+
+  moveOnArea(area, evt) {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    this.setState({
+      moveMsg: `You moved on ${area.shape} ${
+        area.name
+      } at coords ${JSON.stringify(coords)} !`
+    });
+  }
+  
 
 
 }
--- src/index.js
+++ src/index.js
@@ -2,24 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import logo from './logo.svg';
 import * as serviceWorker from './serviceWorker';
 
-const tips = [{
-	text 	: "Tip 1",
-	x		: 22.34,
-	y 		: 20.12
-},{
-	text 	: "Tip 2",
-	x		: 12.34,
-	y 		: 38.12
-},{
-	text 	: "Tip 3",
-	x		: 75.34,
-	y 		: 80.12
-}];
+var MAP = {
+  name: "my-map",
+  areas: [
+    {
+      name: "1",
+      shape: "poly",
+      coords: [25, 33, 27, 300, 128, 240, 128, 94],
+      preFillColor: "green",
+      fillColor: "#0000ff"
+    },
+    {
+      name: "2",
+      shape: "poly",
+      coords: [219, 118, 220, 210, 283, 210, 284, 119],
+      preFillColor: "pink",
+      lineWidth: 10,
+      strokeColor: "#0000ff"
+    },
+    {
+      name: "3",
+      shape: "poly",
+      coords: [381, 241, 383, 94, 462, 53, 457, 282],
+      preFillColor: "yellow", // this is mandatory for stroke color to work
+      lineWidth: 10,
+      strokeColor: "#6afd09"
+    },
+    {
+      name: "4",
+      shape: "poly",
+      coords: [245, 285, 290, 285, 274, 239, 249, 238],
+      preFillColor: "red"
+    },
+    {
+      name: "5",
+      shape: "circle",
+      coords: [170, 100, 25],
+      preFillColor: "rgb(255,255,255,0.3)",
+      lineWidth: 2
+    },
+    {
+      name: "6",
+      shape: "rect",
+      coords: [270, 100, 200, 50],
+      lineWidth: 2,
+      preFillColor: "rgba(255, 255, 255, 0.3)",
+      strokeColor: "#6afd09"
+    }
+  ]
+};
 
-ReactDOM.render(<App img={logo} tips={tips}/>, document.getElementById('root'));
+var URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg";
+
+
+ReactDOM.render(<App img={URL} map={MAP}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
